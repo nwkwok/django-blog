6faf48f1071d3f3c04766f7ef890ca0a501c6373
nwkwok <nwkwok@gmail.com>
2021-01-24 13:23:32 -0800

    update DEBUG_VALUE

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY_PROD')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['nick-django-blog.herokuapp.com']
 
